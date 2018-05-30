fa14a053dfc652dd3dd0482c44bf02466cc8e6fe
wadamasaya <16010418@anabuki-college.ac.jp>
2018-05-30 09:55:08 +0900

    test1

--- app/src/main/java/wadamasaya/listviewsample/ListViewSampleActivity.java
+++ app/src/main/java/wadamasaya/listviewsample/ListViewSampleActivity.java
@@ -2,6 +2,10 @@ package wadamasaya.listviewsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class ListViewSampleActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class ListViewSampleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view_sample);
+        //ListViewオブジェクトを取得。
+        ListView lvMenu = findViewById(R.id.lvMenu);
+        //ListViewにリスナを設定。
+        lvMenu.setOnItemClickListener(new ListItemClickListener());
+    }
+    /*
+    *リストがタップされた時の処理が記述されたメンバクラス
+     */
+    private class ListItemClickListener implements AdapterView.OnItemClickListener{
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view,int position,long id){
+            //タップされた定食名を取得。
+            String item = (String) parent.getItemAtPosition(position);
+            //トーストで表示する文字列を生成
+            String show = "あなたが選んだ定食：" + item;
+            //トーストの表示
+            Toast.makeText(ListViewSampleActivity.this,show,Toast.LENGTH_LONG).show();
+        }
     }
 }
